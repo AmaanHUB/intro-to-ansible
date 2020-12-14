280b1760015524e72e0fc68ca265380224022c6a
Amaan Hashmi-Ubhi <AmaanHUB@gmail.com>
2020-12-14 14:48:09 +0000

    Added awscli installation method

--- ansible-setup/provisioning.sh
+++ ansible-setup/provisioning.sh
@@ -22,7 +22,11 @@ sudo apt install ansible -y
 sudo apt install python3-pip -y
 
 # Install python packages, AWS CLI, boto (as dependency for)
-pip3 install awscli boto boto3
+pip3 install boto boto3
+# installs latest version of aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # set python3 as default
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 20
